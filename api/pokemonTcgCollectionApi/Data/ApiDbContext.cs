a05bc38df23aa6f67564abbeb43ecd33b2e5ecb5
Guilherme Reis <guilherme.reis@sys10.me>
2021-07-19 23:27:09 -0300

    Recriação da API

---
+++ api/pokemonTcgCollectionApi/Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pokemonTcgCollectionApi.Models;
+
+namespace pokemonTcgCollectionApi.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public virtual DbSet<RefreshToken> RefreshTokens { get; set; }
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
